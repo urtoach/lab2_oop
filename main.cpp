fa290c3d5317da4158b0b35cf17af31a1c92d9ed
urtoach <115491314+urtoach@users.noreply.github.com>
2023-09-27 08:36:28 +0000

    getNum

--- main.cpp
+++ main.cpp
@@ -1,5 +1,36 @@
 #include <iostream>
+#include <cstring>
+#include <iostream>
+#include <limits>
 #include <string>
+
+template <class T>
+
+T getNum(T min = std::numeric_limits<T>::min(), T max = std::numeric_limits<T>::max()) {
+  T val;
+  while (true) {
+    std::cin >> val;
+    if (std::cin.eof()) {
+      throw std::runtime_error("find eof");
+    } 
+    else if (std::cin.bad()) {
+      throw std::runtime_error(std::string("failed to read number: ") + strerror(errno));
+    } 
+    else if (std::cin.fail()) {
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+      std::cout << "error. repeat" << std::endl;
+    } 
+    else if (val >= min && val <= max) {
+      return val;
+    }
+    else{
+      std::cin.clear();
+      std::cin.ignore();
+      std::cout << "error. repeat" << std::endl;
+    }
+  }
+}
 
 class SignalState {
 private:
@@ -12,8 +43,8 @@ public:
     this->time = time;
   }
   SignalState(std::string signal){
-    if (signal.find_first_not_of("01") == std::string::npos){
-      std::cout << "fail" << std::endl;
+    if (signal.find_first_not_of("01") != std::string::npos){
+      throw std::invalid_argument("error: invalid characters in string");
     }
     else{
       for (int i = 0; i < signal.length(); i++) {
@@ -35,24 +66,43 @@ public:
     if (duration > 0){
       time += duration;
     }
+    else{
+      throw std::invalid_argument("error: invalid time value");
+    }
   }
 
   void truncateSignal(int duration){
     if (duration > 0 && duration <= time){
       time -= duration;
     }
+    else{
+      throw std::invalid_argument("error: invalid time value");
+    }
   }
 
   std::string formatSignal(){
+    if (time == 0){ return "x"; }
     std::string chart;
     char symbol = (level ? '-' : '_'); 
     for (int i = 0; i < time; i++){
       chart += symbol;
     }
+    return chart;
   }
   
 };
 
 int main(){
-
+  SignalState a;
+  std::cout << a.formatSignal() << std::endl;
+  std::cout << "=========================" << std::endl;
+  SignalState b(0,10);
+  std::cout << b.formatSignal() << std::endl;
+  std::cout << "=========================" << std::endl;
+  std::cout << "enter signal" << std::endl;
+  std::string signal;
+  std::cin >> signal;
+  //std::cout << signal << std::endl;
+  SignalState c(signal);
+  std::cout << c.formatSignal() << std::endl;
 }
